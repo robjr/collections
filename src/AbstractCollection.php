4ae3dc6971e880334607038226e64e9e7ada9444
Roberto Júnior <me@robertojunior.net>
2017-06-19 15:29:19 -0300

    Add doc for methods contains(all) and isEmpty

--- src/AbstractCollection.php
+++ src/AbstractCollection.php
@@ -3,26 +3,31 @@ namespace Collections;
 
 abstract class AbstractCollection implements Collection
 {
-    abstract public function count(): int;
     abstract public function add($element): void;
-    abstract public function remove($element): void;
     abstract public function clear(): void;
+    abstract public function count(): int;
     abstract public function getIterator(): \Iterator;
+    abstract public function remove($element): void;
 
     public function addAll(Collection $collection): void
     {
-        $iterator = $collection->getIterator();
-
-        foreach ($iterator as $object) {
+        foreach ($collection as $object) {
             $this->add($object);
         }
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * Note that $element is present in this collection, if it has the same type and value of at least one element
+     * in this collection. In other words, strict comparison is used to determine if the $element is present.
+     *
+     * @param $element {@inheritdoc}
+     * @return bool {@inheritdoc}
+     */
     public function contains($element): bool
     {
-        $iterator = $this->getIterator();
-
-        foreach ($iterator as $object) {
+        foreach ($this as $object) {
             if ($object === $element) {
                 return true;
             }
@@ -31,11 +36,19 @@ abstract class AbstractCollection implements Collection
         return false;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * Note that each element of $collection is present in this collection, if it has the same type and value of
+     * at least one element in this collection. In other words, strict comparison is used to determine
+     * if an $element is present.
+     *
+     * @param $element {@inheritdoc}
+     * @return bool {@inheritdoc}
+     */
     public function containsAll(Collection $collection): bool
     {
-        $iterator = $collection->getIterator();
-
-        foreach ($iterator as $object) {
+        foreach ($collection as $object) {
             if (!$this->contains($object)) {
                 return false;
             }
@@ -44,6 +57,11 @@ abstract class AbstractCollection implements Collection
         return true;
     }
 
+    /**
+     * {@inheritdoc}
+     *
+     * @return bool {@inheritdoc}
+     */
     public function isEmpty(): bool
     {
         return 0 === $this->count();
--- src/Collection.php
+++ src/Collection.php
@@ -3,11 +3,32 @@ namespace Collections;
 
 interface Collection extends \IteratorAggregate, \Countable
 {
+
     public function add($element): void;
     public function addAll(Collection $collection): void;
     public function clear(): void;
+
+    /**
+     * Returns true if $element is present in the collection.
+     *
+     * @param $element mixed The element that should be present in this collection.
+     * @return bool True if the $element is in the collection. False, otherwise.
+     */
     public function contains($element): bool;
+
+    /**
+     * Returns true if all the elements in $collection is present in this collection.
+     *
+     * @param Collection $collection Collection containing all the elements that should be present in this collection.
+     * @return bool True if the all elements in $collection are in this collection. False, otherwise.
+     */
     public function containsAll(Collection $collection): bool;
+
+    /**
+     * Returns true if there is no elements in this collection.
+     *
+     * @return bool True if this collection contains no elements, false if it contains at least one element.
+     */
     public function isEmpty(): bool;
     public function remove($element): void;
     public function removeAll(Collection $collection): void;
